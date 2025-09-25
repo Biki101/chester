8b2e50553484eb4f6942caaaaf90e9391d13fb88
Biki101 <thakurbiki52@gmail.com>
2025-09-25 14:45:31 +0545

    created board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'chester';
+  rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  columns = ['1', '2', '3', '4', '5', '6', '7', '8'];
 }
