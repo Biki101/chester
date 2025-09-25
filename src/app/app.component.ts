37215bc9415c15c6daf90ff3995a860f8722d0b8
Biki101 <thakurbiki52@gmail.com>
2025-09-25 15:36:04 +0545

    added assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-  columns = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  playingAsWhite = true;
+  columnsWhite = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  rowsWhite = ['8', '7', '6', '5', '4', '3', '2', '1'];
+  columnsBlack = ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+  rowsBlack = ['1', '2', '3', '4', '5', '6', '7', '8'];
 }
